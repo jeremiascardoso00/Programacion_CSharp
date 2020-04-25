f6479b15bf78d82ad55f35e437e23afa7a5c023d
Jeremias Cardoso <jeremiascardoso00@gmail.com>
2020-04-25 05:50:44 -0300

    Ejercicio terminado, agregado el menu en tablero y la invocacion del mismo en el main

--- Jugadores/Jugadores/Program.cs
+++ Jugadores/Jugadores/Program.cs
@@ -41,6 +41,10 @@ namespace JuegoDeApuestas
             El pozo acumula 10000 + 10 -100 = 9910
              */
 
+            Tablero tablero = new Tablero();
+
+            tablero.iteracion();
+
         }
     }
 
@@ -48,12 +52,12 @@ namespace JuegoDeApuestas
     {
 
         private Jugadores[] jugadores;
-        private double pozo;
+        private static int pozo;
         private Dados dado01;
 
         public Tablero()
         {
-            this.pozo = 10.000;
+            pozo = 10000;
             this.dado01 = new Dados();
             this.jugadores = new Jugadores[2];
             this.jugadores[0] = new Jugadores(0);
@@ -64,17 +68,21 @@ namespace JuegoDeApuestas
         
         public void iteracion()
         {
-
+            int i = 0;
             while (this.getPozo() > 0 && jugadores[0].getPlata() > 0  && jugadores[1].getPlata() > 0)
             {
-                
 
-                for (int i = 0; i < 2; i++)
+                int resultado = 0;
+                for (i = 0; i < 2; i++)
                 {
                     int monto = 0;
                     int modo = 0;
                     int apuesta = 0;
                     int resp = 0;
+
+                    //creo una variable para el adicional que crea el hecho de tener distintos modos de apuesta
+                    int cobroPerdida = 0;
+                    int cobroVictoria = 0;
                     do
                     {
                     Console.WriteLine("Jugador {0} ingrese un numero segun el modo de juego que desea:\n" +
@@ -90,15 +98,41 @@ namespace JuegoDeApuestas
                             case 1:
                                 jugadores[i].setModoDeApuesta("Conservador");
                                 resp = 0;
+                                cobroPerdida = 1;
+                                cobroVictoria = 2;
                                 break;
                             case 2:
-                                jugadores[i].setModoDeApuesta("Arriesgado");
-                                resp = 0;
-                                break;
+                                if (jugadores[i].getPlata() > 2)
+                                {
+                                    jugadores[i].setModoDeApuesta("Arriesgado");
+                                    resp = 0;
+                                    cobroPerdida = 2;
+                                    cobroVictoria = 5;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("no podes elegir esta opcion porque no te alcanza la plata");
+                                    resp = 1;
+                                    break;
+                                }
+                                
                             case 3:
-                                jugadores[i].setModoDeApuesta("Desesperado");
-                                resp = 0;
-                                break;
+                                if (jugadores[i].getPlata() > 4)
+                                {
+                                    jugadores[i].setModoDeApuesta("Desesperado");
+                                    resp = 0;
+                                    cobroPerdida = 4;
+                                    cobroVictoria = 15;
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("no podes elegir esta opcion porque no te alcanza la plata");
+                                    resp = 1;
+                                    break;
+                                }
+
                             default:
                                 Console.WriteLine("opcion ingresada invalida");
                                 resp = 1;
@@ -110,14 +144,15 @@ namespace JuegoDeApuestas
                     Console.WriteLine("Jugador {0} ingrese un monto que desee apostar\n", (i + 1));
                     monto = Convert.ToInt16(Console.ReadLine());
                     
-                        if (monto <= jugadores[i].getPlata())
+                        if ((monto * cobroPerdida )<= jugadores[i].getPlata())
                         {
                             jugadores[i].setMontoApuesta(monto);
                             resp = 0;
                         }
                         else
                         {
-                            Console.WriteLine("no cuenta con esa cifra, usted cuenta con ${0}", jugadores[i].getPlata());
+                            Console.WriteLine("no cuenta con esa cifra, usted cuenta con ${0}" +
+                                "\n y usted necesita una cifra mayor o igual al monto que pueda perser segun su tipo de apuesta", jugadores[i].getPlata());
                             resp = 1;
                         }
                     } while (resp == 1);
@@ -139,16 +174,135 @@ namespace JuegoDeApuestas
                         }
                     } while (resp == 1);
 
+                    
                 }
+
+
+                for (i = 0; i < jugadores.Length; i++)
+                {
+                    Console.WriteLine("\njugador {0}\n" +
+                    "apuesta: {1}\n" +
+                    "monto apostado: $ {2}\n" +
+                    "modo de apuesta: {3}", i+1, jugadores[i].getApuesta(), jugadores[i].getMontoApuesta(), jugadores[i].getModoDeApuesta());
+                }
+
+                Console.WriteLine("\ntirando dados...");
+                resultado = dado01.tirar();
+                Console.WriteLine("resultado = {0}", resultado);
+
+                if (resultado == jugadores[0].getApuesta())
+                {
+
+                    if (jugadores[0].getModoDeApuesta() == "Conservador")
+                    {
+                        jugadores[0].setPlata(jugadores[0].getPlata() + (2 * jugadores[0].getMontoApuesta()));
+                        restarPozo(2 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+
+                    }
+                    else if ((jugadores[0].getModoDeApuesta() == "Arriesgado"))
+                    {
+                        jugadores[0].setPlata(jugadores[0].getPlata() + (5 * jugadores[0].getMontoApuesta()));
+                        restarPozo(5 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                    else if ((jugadores[0].getModoDeApuesta() == "Desesperado"))
+                    {
+                        jugadores[0].setPlata(jugadores[0].getPlata() + (15 * jugadores[0].getMontoApuesta()));
+                        restarPozo(15 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+
+                }
+
+                if (resultado != jugadores[0].getApuesta())
+                {
+
+                    if (jugadores[0].getModoDeApuesta() == "Conservador")
+                    {
+                        jugadores[0].setPlata(jugadores[0].getPlata() - (1 * jugadores[0].getMontoApuesta()));
+                        sumarPozo(1 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+
+                    }
+                    else if ((jugadores[0].getModoDeApuesta() == "Arriesgado"))
+                    {
+                        jugadores[0].setPlata(jugadores[0].getPlata() - (2 * jugadores[0].getMontoApuesta()));
+                        sumarPozo(2 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                    else if ((jugadores[0].getModoDeApuesta() == "Desesperado"))
+                    {
+                        jugadores[0].setPlata(jugadores[0].getPlata() - (4 * jugadores[0].getMontoApuesta()));
+                        sumarPozo(4 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                }
+
+                if (resultado == jugadores[1].getApuesta())
+                {
+                    if (jugadores[1].getModoDeApuesta() == "Conservador")
+                    {
+                        jugadores[1].setPlata(jugadores[1].getPlata() + (2 * jugadores[1].getMontoApuesta()));
+                        restarPozo(2 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                    else if ((jugadores[0].getModoDeApuesta() == "Arriesgado"))
+                    {
+                        jugadores[1].setPlata(jugadores[1].getPlata() + (5 * jugadores[1].getMontoApuesta()));
+                        restarPozo(5 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                    else if ((jugadores[0].getModoDeApuesta() == "Desesperado"))
+                    {
+                        jugadores[1].setPlata(jugadores[1].getPlata() + (15 * jugadores[1].getMontoApuesta()));
+                        restarPozo(15 * jugadores[0].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                }
+
+                if (resultado != jugadores[1].getApuesta())
+                {
+                    if (jugadores[1].getModoDeApuesta() == "Conservador")
+                    {
+                        jugadores[1].setPlata(jugadores[1].getPlata() - (1 * jugadores[1].getMontoApuesta()));
+                        sumarPozo(1 * jugadores[1].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                    else if ((jugadores[1].getModoDeApuesta() == "Arriesgado"))
+                    {
+                        jugadores[1].setPlata(jugadores[1].getPlata() - (2 * jugadores[1].getMontoApuesta()));
+                        sumarPozo(2 * jugadores[1].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+                    else if ((jugadores[1].getModoDeApuesta() == "Desesperado"))
+                    {
+                        jugadores[1].setPlata(jugadores[1].getPlata() - (4 * jugadores[1].getMontoApuesta()));
+                        sumarPozo(4 * jugadores[1].getMontoApuesta());
+                        Console.WriteLine("nuevo pozo: {0}", getPozo());
+                    }
+
+                }
+
+                for (i = 0; i < jugadores.Length; i++)
+                {
+                    Console.WriteLine("\njugador {0}\n" +
+                    "plata: {1}\n", i + 1, jugadores[i].getPlata());
+                }
+
             }
         }
-       
-
-
-
-        public double getPozo()
+        public int getPozo()
         {
-            return this.pozo;
+            return pozo;
+        }
+        public void sumarPozo(int monto)
+        {
+            pozo += monto;
+        }
+        public void restarPozo(int monto)
+        {
+            pozo -= monto;
         }
     }
 
@@ -191,23 +345,36 @@ namespace JuegoDeApuestas
         }
         public int getMontoApuesta()
         {
-            return this.apuesta;
+            return this.montoApuesta;
         }
         public void setMontoApuesta(int MontoApuesta)
         {
-            this.montoApuesta = montoApuesta;
+            this.montoApuesta = MontoApuesta;
         }
         public int getPlata()
         {
             return this.plata;
+        }
+        public void setPlata(int plata)
+        {
+            this.plata = plata;
         }
 
     }
 
     class Dados
     {
-        Random r = new Random();
-        public int TirarDados(int valor, int cuenta)
+        /*aca quisieron hacer una funcion recursiva pero malio sal asi que voy a retornar un random y listo*/
+        public int tirar()
+        { 
+            Random r = new Random();
+            int resultado = r.Next(0, 7) + r.Next(0,7);
+            return resultado;
+        }
+       
+
+
+        /*public int TirarDados(int valor, int cuenta)
         {
             int valorSegundoDado = 0;
             Console.WriteLine("Tirando Dado");
@@ -220,7 +387,7 @@ namespace JuegoDeApuestas
 
             }
             return valor;
-        }
+        }*/
     }
 
 }
